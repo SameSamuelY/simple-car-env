758f88f92c143953a38448ffd3eb02e9c72276c2
SameSamuelY <160511490+SameSamuelY@users.noreply.github.com>
2025-05-03 03:57:53 +1000

    Update obstacle.py

--- simple_driving/resources/obstacle.py
+++ simple_driving/resources/obstacle.py
@@ -2,8 +2,8 @@ import pybullet as p
 import os
 
 
-class Goal:
+class Obstacle:
     def __init__(self, client, base):
-        f_name = os.path.join(os.path.dirname(__file__), 'simplegoal.urdf')
-        self.goal = client.loadURDF(fileName=f_name,
+        f_name = os.path.join(os.path.dirname(__file__), 'simpleobstacle.urdf')
+        self.obstacle = client.loadURDF(fileName=f_name,
                    basePosition=[base[0], base[1], 0])
